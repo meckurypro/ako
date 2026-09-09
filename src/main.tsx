f35a8ce44550bb23ecb29bd8efa6456b955a60b6
meckurypro <meckurypro@gmail.com>
2026-09-09 14:29:53 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { ThemeProvider } from './hooks/useTheme.tsx'
+import { SoundProvider } from './hooks/useSound.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <SoundProvider>
+        <App />
+      </SoundProvider>
     </ThemeProvider>
   </StrictMode>,
 )
